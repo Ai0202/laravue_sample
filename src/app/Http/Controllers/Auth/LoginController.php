e03be0d91d0dd561e60ef250ca112d0c823f136a
Atsushi <atsushi88885@gmail.com>
2019-02-25 15:57:22 +0800

    logout実装

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -41,5 +41,12 @@ class LoginController extends Controller
     protected function authenticated(Request $request, $user)
     {
         return $user;
+    }
+
+    protected function loggedOut(Request $request)
+    {
+        $request->session()->regenerate();
+
+        return response()->json();
     }
 }
