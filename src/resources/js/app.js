f3c3d24641b32ff9401aa850bddb9c65b427d223
Atsushi <atsushi88885@gmail.com>
2019-02-25 23:29:25 +0800

    protect for CSRF

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,10 +1,13 @@
+import './bootstrap'
 import Vue from 'vue'
 import router from './router'
+import store from './store'
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router, //define routing
+  store,
   components: { App }, //Declare to use root component
   template: '<App />' //render root component
 })
