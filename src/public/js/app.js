fca8b63c1ae6a8c4aaa88f3cc4e3ddfb8b630ca1
Atsushi <atsushi88885@gmail.com>
2019-03-10 02:22:57 +0900

    写真作成機能

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1956,6 +1956,17 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+//
+//
+//
 //
 //
 //
@@ -1974,6 +1985,74 @@ __webpack_require__.r(__webpack_exports__);
       type: Boolean,
       required: true
     }
+  },
+  data: function data() {
+    return {
+      preview: null,
+      photo: null
+    };
+  },
+  methods: {
+    onFileChange: function onFileChange(event) {
+      var _this = this;
+
+      if (event.target.files.length === 0) {
+        this.reset();
+        return false;
+      }
+
+      if (!event.target.files[0].type.match('image.*')) {
+        this.reset();
+        return false;
+      }
+
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        _this.preview = e.target.result;
+        _this.photo = event.target.files[0];
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    },
+    reset: function reset() {
+      this.preview = '';
+      this.photo = null;
+      this.$el.querySelector('input[type="file"]').value = null;
+    },
+    submit: function () {
+      var _submit = _asyncToGenerator(
+      /*#__PURE__*/
+      _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+        var formData, response;
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                formData = new FormData();
+                formData.append('photo', this.photo);
+                _context.next = 4;
+                return axios.post('/api/photos', formData);
+
+              case 4:
+                response = _context.sent;
+                this.reset();
+                this.$emit('input', false);
+
+              case 7:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function submit() {
+        return _submit.apply(this, arguments);
+      }
+
+      return submit;
+    }()
   }
 });
 
@@ -3639,7 +3718,33 @@ var render = function() {
     [
       _c("h2", { staticClass: "title" }, [_vm._v("Submit a photo")]),
       _vm._v(" "),
-      _vm._m(0)
+      _c(
+        "form",
+        {
+          staticClass: "form",
+          on: {
+            submit: function($event) {
+              $event.preventDefault()
+              return _vm.submit($event)
+            }
+          }
+        },
+        [
+          _c("input", {
+            staticClass: "form__item",
+            attrs: { type: "file" },
+            on: { change: _vm.onFileChange }
+          }),
+          _vm._v(" "),
+          _vm.preview
+            ? _c("output", { staticClass: "form__output" }, [
+                _c("img", { attrs: { src: _vm.preview, alt: "" } })
+              ])
+            : _vm._e(),
+          _vm._v(" "),
+          _vm._m(0)
+        ]
+      )
     ]
   )
 }
@@ -3648,16 +3753,12 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("form", { staticClass: "form" }, [
-      _c("input", { staticClass: "form__item", attrs: { type: "file" } }),
-      _vm._v(" "),
-      _c("div", { staticClass: "form__button" }, [
-        _c(
-          "button",
-          { staticClass: "button button--inverse", attrs: { type: "submit" } },
-          [_vm._v("submit")]
-        )
-      ])
+    return _c("div", { staticClass: "form__button" }, [
+      _c(
+        "button",
+        { staticClass: "button button--inverse", attrs: { type: "submit" } },
+        [_vm._v("submit")]
+      )
     ])
   }
 ]
--- src/resources/js/components/PhotoForm.vue
+++ src/resources/js/components/PhotoForm.vue
@@ -1,8 +1,11 @@
 <template>
   <div v-show="value" class="photo-form">
     <h2 class="title">Submit a photo</h2>
-    <form class="form">
-      <input class="form__item" type="file">
+    <form class="form" @submit.prevent="submit">
+      <input class="form__item" type="file" @change="onFileChange">
+      <output class="form__output" v-if="preview">
+        <img :src="preview" alt="">
+      </output>
       <div class="form__button">
         <button type="submit" class="button button--inverse">submit</button>
       </div>
@@ -17,6 +20,47 @@ export default {
             type: Boolean,
             required: true
         }
+    },
+    data () {
+      return {
+        preview: null,
+        photo: null
+      }
+    },
+    methods: {
+      onFileChange (event) {
+        if (event.target.files.length === 0) {
+          this.reset()
+          return false
+        }
+
+        if (! event.target.files[0].type.match('image.*')) {
+          this.reset()
+          return false
+        }
+
+        const reader = new FileReader()
+
+        reader.onload = e => {
+          this.preview = e.target.result
+          this.photo = event.target.files[0]
+        }
+
+        reader.readAsDataURL(event.target.files[0]);
+      },
+      reset () {
+        this.preview = ''
+        this.photo = null
+        this.$el.querySelector('input[type="file"]').value = null
+      },
+      async submit () {
+        const formData = new FormData()
+        formData.append('photo', this.photo)
+        const response = await axios.post('/api/photos', formData)
+
+        this.reset()
+        this.$emit('input', false)
+      }
     }
 }
 </script>
