1a780c9760578b9f1d9256db6c1d190136ff6200
Atsushi <atsushi88885@gmail.com>
2019-03-10 01:18:58 +0900

    写真投稿API

--- src/routes/api.php
+++ src/routes/api.php
@@ -19,6 +19,8 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
