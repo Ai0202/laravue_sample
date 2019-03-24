062990a6a6f1add4857c5b5483dce9e233faf977
Atsushi <atsushi88885@gmail.com>
2019-03-24 16:57:53 +0900

    vuex導入, CSRF対策

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import router from './router'
+import store from './store'
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router, //define routing
+  store,
   components: { App }, //Declare to use root component
   template: '<App />' //render root component
 })
